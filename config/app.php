8dc5c0129cf2315d06283b8a07aabadf5cbcc24b
muradhajimuradov <muradhajimuradov@gmail.com>
2020-05-04 19:24:49 +0400

    Log-viewer

--- config/app.php
+++ config/app.php
@@ -174,7 +174,8 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-
+        /* LogViewer */
+        Arcanedev\LogViewer\LogViewerServiceProvider::class,
     ],
 
     /*
