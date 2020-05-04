1b5765038b0e3f4e7eae3aa7ac9732d4e2a22fec
Zemav <imammadov34@gmail.com>
2020-05-04 19:29:36 +0400

    'Posts'

---
+++ resources/views/post/show.blade.php
@@ -0,0 +1,30 @@
+@extends('layouts.app')
+<style>
+.card {
+  background-color: white;
+  padding: 20px;
+  margin-top: 20px;
+}
+.card .card-content{
+    font-size: 20px;
+}
+.card h2{
+    text-decoration: underline;
+}
+.card .card-date{
+    text-align: right;
+}
+</style>
+@section('content')
+<div class="card">
+      <h2 style="text-align: center">{{ $post->title }}</h2>
+      <p class="card-content">{{$post->content}}</p>
+      <div class="card-date">
+      <p>Created: <?= date_format($post->created_at, "d F Y (H:i)") ?></p>
+      <?php 
+        if ($post->updated_at != $post->created_at) : ?>
+        <p>Updated: <?= date_format($post->updated_at, "d F Y (H:i)") ?></p>
+        <?php endif;?>
+      </div>
+</div>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -18,5 +18,7 @@ Route::get('/', function () {
 });
 
 Auth::routes();
-
+Route::group(['middleware' => ['auth']], function (){
 Route::get('/home', 'HomeController@index')->name('home');
+Route::resource('/post', 'PostController');
+});
